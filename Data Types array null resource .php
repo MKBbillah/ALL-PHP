55ec6fe2d65008dee330912068faeb89af26c460
Mukarram billah <103162305+MKBbillah@users.noreply.github.com>
2024-03-17 11:32:44 +0600

    Add files via upload
    Resource is not data type

---
+++ Data Types array null resource .php
@@ -0,0 +1,25 @@
+<?php
+
+/*Data Type
+1.String
+2.Integer
+3.Float
+4.Boolean
+5.Array
+6.Object
+7.Null
+8.Resource 
+*/
+
+//Array
+$person_infos = array("01406440604","Mukarram", "Khulna");
+
+var_dump($person_infos);
+
+//Null
+
+$name=null;
+var_dump($name);
+
+
+?>
