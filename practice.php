f76af4a2f3980e8ff76ef2f38c13a57584c69381
Mukarram billah <103162305+MKBbillah@users.noreply.github.com>
2024-03-17 10:57:24 +0600

    Data type name
    Global variable done

---
+++ practice.php
@@ -0,0 +1,23 @@
+<?php
+
+$GLOBALS;
+
+$person_name = "Mukarram";
+
+function getName(){
+    echo $GLOBALS['person_name'];
+}
+
+getName();
+
+/* Datatype
+1. String
+2. Integer
+3. Float
+4. Boolean
+5. Array
+6. Object
+7. Null
+8. Resource
+*/
+?>
