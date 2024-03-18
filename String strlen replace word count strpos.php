714fe27c953e855ade18dbda0f78e74453a77705
Mukarram billah <103162305+MKBbillah@users.noreply.github.com>
2024-03-18 11:48:19 +0600

    Add files via upload
    string length, replace, word count string position.

---
+++ String strlen replace word count strpos.php
@@ -0,0 +1,29 @@
+<?php
+
+// string length
+$username = "Mukarram";
+
+echo strlen($username);
+
+//str_word_count
+$short_desc = "Mukarram";
+
+echo str_word_count($short_desc);
+
+//strrev
+$username = "Mukarram";
+
+echo strrev($username);
+
+//strpos
+$username = "Mukarram";
+
+$para = "is a boy. also boy";
+
+//echo strpos($para,$username);
+
+//str_replace
+
+echo str_replace($para, "billah", "jaman is good boy");
+
+?>
