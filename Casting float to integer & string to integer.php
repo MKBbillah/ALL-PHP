1d3053b887bfbe2f59d38fc73ec3859d4425e8e7
Mukarram billah <103162305+MKBbillah@users.noreply.github.com>
2024-03-19 10:29:37 +0600

    Add files via upload
    changing int to str.

---
+++ Casting float to integer & string to integer.php
@@ -0,0 +1,15 @@
+<?php
+
+//$price = 500.321;
+//$price_cost= (int)$price;
+
+//echo $price_cost;
+
+$price = "210234.814";
+var_dump($price);
+
+$price_string_to_int = (int)$price;
+
+var_dump(is_int($price_string_to_int));
+
+?>
