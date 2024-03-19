8767330822adef816c539eeeebaa95baefab6e7d
Mukarram billah <103162305+MKBbillah@users.noreply.github.com>
2024-03-19 10:14:33 +0600

    Add files via upload
    built integer.

---
+++ integer-built.php
@@ -0,0 +1,21 @@
+<?php
+
+// PHP_INT_MAX();
+// PHP_INT_MIN();
+// PHP_INT_SIZE();
+
+// is_int()
+
+// is_integer() alias is_int()
+
+// is_long() 
+
+$product_price = 5000;
+
+var_dump(is_int($product_price));
+
+$price = 5;
+
+var_dump(is_float($price));
+
+?>
