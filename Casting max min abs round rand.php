52675024ca188868787c4280f84da511c3f11e54
Mukarram billah <103162305+MKBbillah@users.noreply.github.com>
2024-03-19 11:21:24 +0600

    Add files via upload
    rand = random number, abs = - to +

---
+++ Casting max min abs round rand.php
@@ -0,0 +1,21 @@
+<?php
+
+echo max(1,2,10,52,45,12,85,102,10,1);
+
+$max_number = max(1,2,10,52,45,12,85,102,10,1);
+
+$min_number = min(1,2,10,52,45,12,85,102,10,1);
+
+echo $min_number;
+
+$number = -54.263;
+
+echo round($number);
+
+echo abs($number);
+
+echo sqrt(64);
+
+echo rand(10,50);
+
+?>
