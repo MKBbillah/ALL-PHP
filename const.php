e34e27a4f6fd325cf9b1966f5fe25e9b1928785d
Mukarram billah <103162305+MKBbillah@users.noreply.github.com>
2024-03-20 11:26:13 +0600

    Add files via upload
    define, const, array use

---
+++ const.php
@@ -0,0 +1,26 @@
+<?php
+
+define("message","Welcome to PHPLearn");
+
+const name = "value";
+
+echo name;
+
+function getName(){
+    echo message;
+}
+
+getName();
+
+//echo name;
+
+define('info',[
+    'Mukarram',
+    '01406440604',
+    'Khulna'
+]);
+
+var_dump(info[0]);
+echo info[1];
+
+?>
