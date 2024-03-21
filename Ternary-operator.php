8cbde3b4ebd137786576fb183096aa70f1960eba
Mukarram billah <103162305+MKBbillah@users.noreply.github.com>
2024-03-21 10:57:20 +0600

    Add files via upload

---
+++ Ternary-operator.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+$age = 10;
+$getresult = $age>18?true:"false";
+
+echo $getresult;
+print("$getresult");
+?>
+
+</body>
+</html>
