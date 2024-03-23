95ff812479e90492835d04763ed2f29a2121e867
Mukarram billah <103162305+MKBbillah@users.noreply.github.com>
2024-03-23 10:24:48 +0600

    Add files via upload

---
+++ Loop.php
@@ -0,0 +1,25 @@
+<?php
+
+//$video = 100;
+
+//while(condition is true){
+  //  this block executed
+//}
+
+//next line jete parbe na
+
+$starting = 0;
+while($starting<=100){
+    echo "Porte Thakbo $starting </br>";
+    $starting+=1;
+}
+
+echo "Next Line";
+
+$init = 0;
+while($init<=10){
+    echo $init;
+    $init+=1;
+}
+
+?>
