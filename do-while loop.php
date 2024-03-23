93c0ba2dbca9484532e7bdbe37b1952df4886565
Mukarram billah <103162305+MKBbillah@users.noreply.github.com>
2024-03-23 10:36:34 +0600

    Add files via upload

---
+++ do-while loop.php
@@ -0,0 +1,18 @@
+<?php
+
+//$post=20;
+//$init=0;
+//while($init<=$post){
+ // echo "This is my Post".$init."</br>";
+  //$init++;
+//}
+
+$start_point = 1;
+do{
+  echo "This is start Point $start_point </br>";
+  $start_point+=2;
+}
+while($start_point<=20)
+
+
+?>
