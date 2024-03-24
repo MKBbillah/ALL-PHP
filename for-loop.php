9c0ab0a886716a3aeaf7189126dae59e199d01b2
Mukarram billah <103162305+MKBbillah@users.noreply.github.com>
2024-03-24 11:16:01 +0600

    Add files via upload

--- for-loop.php
+++ for-loop.php
@@ -1,31 +1,11 @@
 <?php
 
-//$post=20;
-//$init=0;
-//while($init<=$post){
- // echo "This is my Post".$init."</br>";
-  //$init++;
+
+//$infos = array(1,2,3,5,5,5,58,58,8,9,98,9,52);
+//foreach($infos as $single_value){
+  // echo $single_value."</br>";
 //}
 
-$info =array("Mukarram","01406440604", "01");
-
-$count_array_value = count($info);
-
-
-//var_dump($info);
-for($i=0;$i<$count_array_value; $i++){
-  echo $i."</br>";
-  echo $info[$i]."</br>";
-}
-/*
-echo $count_array_value;
-exit();
-
-$post=20;
-$init=0;
-for($init=0;$init<=$post;$init++){
-  echo "ami Gurichi ".$init."bar</br>";
-}*/
-
+foreach($array_variable as $single_variable)
 
 ?>
