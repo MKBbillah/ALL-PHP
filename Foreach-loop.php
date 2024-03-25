cba879210104eab189df47d1f91665f738af9014
Mukarram billah <103162305+MKBbillah@users.noreply.github.com>
2024-03-25 10:27:28 +0600

    Add files via upload

---
+++ Foreach-loop.php
@@ -0,0 +1,11 @@
+<?php
+
+
+//$infos = array(1,2,3,5,5,5,58,58,8,9,98,9,52);
+//foreach($infos as $single_value){
+  // echo $single_value."</br>";
+//}
+
+foreach($array_variable as $single_variable)
+
+?>
