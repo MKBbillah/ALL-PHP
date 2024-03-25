040b9efa09baa8ed2383ebab52d68554e9584d13
Mukarram billah <103162305+MKBbillah@users.noreply.github.com>
2024-03-25 11:36:21 +0600

    Add files via upload
    admit, lib, shop all are connected

---
+++ admit.php
@@ -0,0 +1,11 @@
+<?php
+
+include("lib.php");
+
+$first_name='m';
+$last_name= 'j';
+
+echo getFulName($first_name,$last_name);
+
+
+?>
---
+++ shop.php
@@ -0,0 +1,10 @@
+<?php
+
+include("lib.php");
+
+$price=10;
+$qty=20;
+echo PriceCalculate(10,20);
+
+
+?>
