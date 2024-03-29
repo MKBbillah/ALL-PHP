8ec642a574c13bd82d6685e10437bf958ee09f96
Mukarram billah <103162305+MKBbillah@users.noreply.github.com>
2024-03-29 12:10:22 +0600

    Add files via upload

---
+++ superglobal-server.php
@@ -0,0 +1,17 @@
+<?php
+
+//echo $_SERVER['SERVER_ADDR'];
+
+echo $_SERVER['SERVER_NAME'];
+
+echo $_SERVER['SERVER_SOFTWARE'];
+echo $_SERVER['REMOTE_ADDR'];
+echo $_SERVER['SERVER_METHOD'];
+echo $_SERVER['REMOTE_PORT'];
+echo $_SERVER['SCRIPT_NAME'];
+echo $_SERVER['PHP_SELF'];
+echo $_SERVER['HTTP_HOST'];
+echo $_SERVER['PHP_REFERER'];
+echo $_SERVER['PHP_USER_AGENT'];
+
+?>
