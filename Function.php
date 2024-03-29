c21c23b3307cb9af95dabfcbadafda98ee0edf76
Mukarram billah <103162305+MKBbillah@users.noreply.github.com>
2024-03-29 11:07:43 +0600

    Add files via upload

---
+++ Function.php
@@ -0,0 +1,18 @@
+<?php
+
+
+///function functionName(){
+  //echo "Mukarram";
+//}
+
+//functionName();
+
+Function Price_Calculate(){
+
+  $product_price =100;
+  $product_qty = 1;
+  $total_price = $product_price*$product_qty;
+  return $total_price;
+}
+
+?>
