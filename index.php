d2030744fcf79b19d047875735967cac2301f4c6
Mukarram billah <103162305+MKBbillah@users.noreply.github.com>
2024-04-26 11:41:43 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+ <?php
+ // include "header.php";
+  include_once("header.php");
+
+ //require "header.php";
+ //require_once("header.php");
+ ?> 
+<div class="container-fluid mt-3">
+<h2>Three Equal</h2>
+<p>You can also use numbers to control the column width. Just make sure that the sum always</p>
+<div class="row">
+    <div class="col-4 bg-success">.col-4</div>
+    <div class="col-4 bg-warning">.col-4</div>
+    <div class="col-4 bg-success">.col-4</div>
+</div>
+</div>
+
